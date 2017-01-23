5b0c42fe471989603ef74b8fb68d1b0bc5213ef9
TianT <hm14303980@outlook>
2017-01-23 00:46:13 +0000

    Init commit

---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.default')
+@section('title','帮助')
+@section('content')
+<<<<<<< HEAD
+    <h1>这里是帮助页1</h1>
+=======
+    <h1>这里是帮助页</h1>
+>>>>>>> e1971daf4025b0e1b80ddea9b518efe0653456a1
+@stop
