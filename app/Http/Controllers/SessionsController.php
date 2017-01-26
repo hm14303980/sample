3c48797f37d02d5ec7805f49ff4191111afd53c1
TianT <hm14303980@outlook>
2017-01-26 02:29:23 +0000

    Finish login and logout

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use Auth;
+
+class SessionsController extends Controller
+{
+    public function create(){
+        return view('sessions.create');
+    }
+
+    public function store(Request $request){
+        $this->validate($request,[
+           'email'=>'required|email|max:255',
+            'password'=>'required'
+        ]);
+
+        $credentials = [
+            'email'=>$request->email,
+            'password'=>$request->password,
+        ];
+
+        if(auth::attempt($credentials,$request->has('remember'))){
+            session()->flash('success','欢迎回来！');    //登录成功后的相关操作
+            return redirect()->route('users.show',[Auth::user()]);
+        }else{
+            session()->flash('danger','很对不住，你的邮箱和密码不正确！');  //登录失败后的相关操作
+            return redirect()->back();
+        }
+
+        return;
+    }
+
+    public function destroy(){
+        Auth::logout();
+        session()->flash('success','您已经成功退出！');
+        return redirect('login');
+    }
+}
